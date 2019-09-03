cbbb63cc362fda390d42c26e77d460784353dd05
yalany <nicky_evil@mail.ru>
2019-09-04 01:18:22 +0300

    Commands behavior

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,7 +5,44 @@ public class User {
     private int id;
     private int gamesFinished;
     private boolean haveActiveGame;
+    private long subscriptionEndTime;
 
-    private boolean isSubscribed;
-    private long expirationDate;
+    public User(int id) {
+        this.id = id;
+        gamesFinished = 0;
+        haveActiveGame = false;
+        subscriptionEndTime = System.currentTimeMillis();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGamesFinished() {
+        return gamesFinished;
+    }
+
+    public boolean isHaveActiveGame() {
+        return haveActiveGame;
+    }
+
+    public boolean isSubscribed() {
+        return System.currentTimeMillis() < subscriptionEndTime;
+    }
+
+    public long getSubscriptionEndTime() {
+        return subscriptionEndTime;
+    }
+
+    public void incrementGamesFinished() {
+        this.gamesFinished++;
+    }
+
+    public void setHaveActiveGame(boolean haveActiveGame) {
+        this.haveActiveGame = haveActiveGame;
+    }
+
+    public void setSubscriptionEndTime(long subscriptionEndTime) {
+        this.subscriptionEndTime = subscriptionEndTime;
+    }
 }
