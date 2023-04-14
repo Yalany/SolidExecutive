40f6816b374a338a21497630b2fc3055fec9d0cd
Nik Dobrinskiy <Yalany@users.noreply.github.com>
2023-04-15 02:48:46 +0300

    Update SessionOperator.java

--- src/main/java/model/session/SessionOperator.java
+++ src/main/java/model/session/SessionOperator.java
@@ -2,21 +2,21 @@ package model.session;
 
 public final class SessionOperator {
 
-    private final Session context;
+    private final Session session;
 
-    SessionOperator(final Session context) {
-        this.context = context;
+    SessionOperator(final Session session) {
+        this.session = session;
     }
 
     public void scheduleEvent(int eventId, int offset) {
-        context.getEventDeck().shuffleIn(eventId, offset);
+        session.getEventDeck().shuffleIn(eventId, offset);
     }
 
     public void insertNextEvent(int eventId) {
-        context.getEventDeck().putOnTop(eventId);
+        session.getEventDeck().putOnTop(eventId);
     }
 
     public void changeResources(String resourceName, int resourceAmount) {
-        context.getGameResources().modifyAmount(resourceName, resourceAmount);
+        session.getGameResources().modifyAmount(resourceName, resourceAmount);
     }
 }
